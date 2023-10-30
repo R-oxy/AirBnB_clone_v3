1468fe3cdf5605692ba559286f5eb6f39c2f13df
R-oxy <aronmangati@gmail.com>
2023-10-30 05:23:01 -0700

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
