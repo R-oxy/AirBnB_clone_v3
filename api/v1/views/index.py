c38881279f3d979bb8adcb654bfeaa3cafd6ed8a
R-oxy <aronmangati@gmail.com>
2023-10-30 06:37:35 -0700

    Fixed Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-from api.v1.views import app_views, storage
+from api.v1.views import (
+    app_views, storage, Amenity, City, Place,
+    Review, State, User
+)
 from flask import jsonify
 
 
@@ -12,12 +15,14 @@ def get_status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     """Retrieves the number of each object by type"""
-    objects = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(objects)
+    objects = {User: "users",
+               Amenity: "amenities",
+               City: "cities",
+               Place: "places",
+               Review: "reviews",
+               State: "states"}
+
+    stats = {}
+    for cls in objects.keys():
+        stats[objects[cls]] = storage.count(cls)
+    return jsonify(stats)
